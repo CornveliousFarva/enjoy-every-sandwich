1c0cc70eec5837c992d0e1dc647ad022bfaa9b17
Greg Schuman <gcschuman@hotmail.com>
2023-09-26 14:22:57 -0700

    Added a new class - navbarDark on the navbar scroll, which adds a dark background color to the navbar when scrolling down the page.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Adding class navbarDark on navbard acroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function(){
+    var top = window.scrollY;
+    if(top >= 100){
+        header.classList.add('navbarDark')
+    }
+    else{
+        header.classList.remove('navbarDark')
+    }
+}
+
 // Dark mode script
 function darkMode() {
     const element = document.body;
